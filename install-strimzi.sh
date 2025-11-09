66759d67aedcada85bf17e6fb9d93dda39c68efc
Manoj Lamichhane <manoj.lamichhane@sita.aero>
2025-11-08 21:52:54 -0500

    Migrating from Zookeeper to Kraft

--- install-strimzi.sh
+++ install-strimzi.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 kubectl create namespace kafka
 kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
+kubectl apply -f controller.yaml -n kafka
 kubectl apply -f kafka-cluster-ephemeral.yaml -n kafka
 kubectl wait kafka/my-cluster --for=condition=Ready --timeout=300s -n kafka
