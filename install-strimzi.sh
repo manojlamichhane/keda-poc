aa69363b8f01f453fc1cff9b6ca18c47f6f71fe1
Manoj Lamichhane <manoj.lamichhane@sita.aero>
2025-11-11 10:09:23 -0500

    Optimizing the scaling

--- install-strimzi.sh
+++ install-strimzi.sh
@@ -3,4 +3,4 @@ kubectl create namespace kafka
 kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
 kubectl apply -f controller.yaml -n kafka
 kubectl apply -f kafka-cluster-ephemeral.yaml -n kafka
-kubectl wait kafka/my-cluster --for=condition=Ready --timeout=300s -n kafka
+kubectl wait kafka/my-cluster --for=condition=Ready --timeout=360s -n kafka
